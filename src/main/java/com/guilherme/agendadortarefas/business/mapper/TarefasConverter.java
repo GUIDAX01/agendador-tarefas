fe88ff5488e8edab9f98d044344469b5eb46e498
Guilherme Macena <166922523+GUIDAX01@users.noreply.github.com>
2026-07-21 22:50:06 -0300

    adicionados métodos Get por email e por período

--- src/main/java/com/guilherme/agendadortarefas/business/mapper/TarefasConverter.java
+++ src/main/java/com/guilherme/agendadortarefas/business/mapper/TarefasConverter.java
@@ -4,10 +4,16 @@ import com.guilherme.agendadortarefas.business.dto.TarefasDTO;
 import com.guilherme.agendadortarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface TarefasConverter {
 
     TarefasEntity paraTarefaEntity(TarefasDTO dto);
 
     TarefasDTO paraTarefasDTO(TarefasEntity entity);
+
+    List<TarefasEntity> paraListaTarefaEntity(List<TarefasDTO> dtos);
+
+    List<TarefasDTO> paraListaTarefasDTO(List<TarefasEntity> entities);
 }
--- src/main/java/com/guilherme/agendadortarefas/controller/TarefasController.java
+++ src/main/java/com/guilherme/agendadortarefas/controller/TarefasController.java
@@ -3,8 +3,12 @@ package com.guilherme.agendadortarefas.controller;
 import com.guilherme.agendadortarefas.business.TarefasService;
 import com.guilherme.agendadortarefas.business.dto.TarefasDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tarefas")
@@ -18,4 +22,19 @@ public class TarefasController {
                                                     @RequestHeader("Authorization") String token){
         return ResponseEntity.ok(tarefasService.gravarTarefa(token, dto));
     }
+
+    @GetMapping("/eventos")
+    public ResponseEntity<List<TarefasDTO>> buscarListaDeTarefasPorPeriodo(
+            @RequestParam @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm:ss") LocalDateTime dataInicial,
+            @RequestParam @DateTimeFormat(pattern = "dd-MM-yyyy HH:mm:ss") LocalDateTime dataFinal){
+
+        return ResponseEntity.ok(tarefasService.buscaTarefasAgendadasPorPeriodo(dataInicial, dataFinal));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TarefasDTO>> buscarTarefasPorEmail(@RequestHeader("Authorization") String token){
+
+        return ResponseEntity.ok(tarefasService.buscaTarefasPorEmail(token));
+    }
+
 }
